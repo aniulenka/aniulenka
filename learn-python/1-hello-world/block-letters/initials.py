4d11d5cb816f7dade276d248041a8b32665cc423
aniulenka <36196520+aniulenka@users.noreply.github.com>
2021-07-05 23:26:42 +0200

    Create initials.py

---
+++ learn-python/1-hello-world/block-letters/initials.py
@@ -0,0 +1,10 @@
+# Anna B
+# this are my initials
+
+print("  A    BBBB ")
+print(" A A   B   B")
+print("A   A  B   B")
+print("AAAAA  BBBB ")
+print("A   A  B   B")
+print("A   A  B   B")
+print("A   A  BBBB ")
